b50596e3cf3645822a4a55e3bd2558280da6d550
fabioportovasques <fabioportovasques321@gmail.com>
2020-07-19 03:33:24 -0300

    alteração index.php

--- segware-epi/index.php
+++ segware-epi/index.php
@@ -22,28 +22,32 @@
         <div class="marcador col-md-6 col-xl-6">
            <div class="card">
               <div class="card-body">
-                <h4 class="card-title">Título do cartão</h4>
-                <h6 class="card-subtitle">Subtítulo do cartão</h6>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sapien dui, feugiat et lorem quis, fringilla maximus nisl. Vivamus sed est pulvinar, aliquet purus ut, dictum elit.</p>
+                <h4 class="card-title">EQUIPAMENTO DE PROTEÇÃO INDIVIDUAL</h4>
+                <h6 class="card-subtitle">E.P.I</h6>
+                <p class="card-text">O Sistema tem como objetivo facilitar o gerenciamento dos E.P.IS pela empresa, além de tornar mais seguro o dia a dia dos seus colaboradores, com análise completa de cada tarefa executada..</p>
                 <a class="btn btn-outline-primary" href="">Leia mais</a>
               </div>
-              <img class="card-img-bottom" src="https://source.unsplash.com/random/600x200">
+              <img class="card-img-bottom" src="img/epi.jpg">
             </div>
         </div>
       
       <div class="marcador2 col-md-4 col-xl-4">
-          <img src="img/logo-atual.png" class="img-responsive">
-          <form>
-            <div class="form-group">
-              <input type="email" class="form-control form-control-lg" style="min-width:100px" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Sua Matricula">
-              
-            </div>
-            <div class="form-group">
-              <input type="password" class="form-control form-control-lg"  id="exampleInputPassword1" placeholder="Senha">
-            </div>
-           
-            <button type="submit" class="btn btn-success active btn-block">Entrar</button>
-          </form>
+        <div class="card">
+          <div class="card-body">
+              <img src="img/logo-atual.png" class="img-responsive">
+              <form>
+                <div class="form-group">
+                  <input type="email" class="form-control form-control-lg" style="min-width:100px" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Sua Matricula">
+                  
+                </div>
+                <div class="form-group">
+                  <input type="password" class="form-control form-control-lg"  id="exampleInputPassword1" placeholder="Senha">
+                </div>
+               
+                <button type="submit" class="btn btn-success active btn-block">Entrar</button>
+              </form>
+           </div>
+           </div>   
         </div>
 
 
