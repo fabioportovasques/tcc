e6221f61f4d528b9c7c73a247b88934bf0c95a2e
fabioportovasques <fabioportovasques321@gmail.com>
2020-07-19 00:42:55 -0300

    add criação do layout

---
+++ segware-epi/index.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    
+     <link rel="stylesheet" href="bootstrap4/css/bootstrap.min.css">
+     <link rel="stylesheet" href="bootstrap4/css/style.css">
+
+    
+
+    <title>Segware EPI</title>
+  </head>
+  <body>
+
+
+    <div class="container marcador-container">
+      <div class="row">
+        <div class="marcador col-md-6 col-xl-6">
+          <button class="btn btn-warning">Leia mais</button>
+        </div>
+      
+      <div class="marcador2 col-md-4 col-xl-4">
+
+          <form>
+            <div class="form-group">
+              <input type="email" class="form-control form-control-lg" style="min-width:100px" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Seu email">
+              
+            </div>
+            <div class="form-group">
+              <input type="password" class="form-control form-control-lg"  id="exampleInputPassword1" placeholder="Senha">
+            </div>
+           
+            <button type="submit" class="btn btn-primary">Enviar</button>
+          </form>
+        </div>
+
+
+
+              <div class="marcador2 col-md-4 col-xl-4">
+
+          
+            </div>
+      </div>
+    </div>
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
