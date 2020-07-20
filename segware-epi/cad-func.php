d25a7d7a30c9bf581a4d1e07b963c7cade01dfc9
fabioportovasques <fabioportovasques321@gmail.com>
2020-07-20 16:38:42 -0300

    add arquivo dashboard

---
+++ segware-epi/cad-func.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    
+     <link rel="stylesheet" href="bootstrap4/css/bootstrap.min.css">
+     <link rel="stylesheet" href="bootstrap4/css/style.css">
+
+    
+
+    <title>Cadastro De Funcinários</title>
+  </head>
+  <body>
+
+
+    <div class="container marcador-container ">
+      <div class="row">
+        <div class="marcador col-md-6 col-xl-6">
+           <div class="card">
+              <div class="card-body">
+                <h4 class="card-title">SISTEMA DE GESTÃODE EPI</h4>
+                <h6 class="card-subtitle">SEGWARE EPI</h6>
+                <p class="card-text">O Sistema tem como objetivo facilitar o gerenciamento dos E.P.IS pela empresa, além de tornar mais seguro o dia a dia dos seus colaboradores, com análise completa de cada tarefa executada..</p>
+                <a class="btn btn-outline-primary" href="home_introducao.php">Leia mais</a>
+              </div>
+              <img class="card-img-bottom" src="img/epi.jpg">
+            </div>
+        </div>
+      
+      <div class="marcador2 col-md-4 col-xl-4">
+        <div class="card">
+          <div class="card-body">
+              <img src="img/logo3.png" class="img-responsive">
+              <form>
+                <div class="form-group">
+                  <input type="email" class="form-control form-control-lg" style="min-width:100px" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Sua Matricula">
+                  
+                </div>
+                <div class="form-group">
+                  <input type="password" class="form-control form-control-lg"  id="exampleInputPassword1" placeholder="Senha">
+                </div>
+                <a href="#" class="badge badge-light">Esqueceu sua senha?</a>
+                 <br />
+                 <br />
+                <button type="submit" class="btn btn-success active btn-block">Entrar</button>
+              </form>
+           </div>
+           </div>   
+        </div>
+
+
+
+              <div class="marcador2 col-md-4 col-xl-4">
+
+          
+            </div>
+      </div>
+    </div>
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
--- segware-epi/home_introducao.php
+++ segware-epi/home_introducao.php
@@ -23,9 +23,8 @@
 		  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 		  <a class="btn btn-primary btn-lg" href="index.php" role="button">Voltar</a>
 
-		  <p class="text-right">Desenvolvido por Fábio Vasques. <br />contato: fabioportovasques321@gmail.com fone:<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-phone" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-		  <path fill-rule="evenodd" d="M11 1H5a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z"/>
-		  <path fill-rule="evenodd" d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+		  <p class="text-right">Desenvolvido por Fábio Vasques. <br />contato: fabioportovasques321@gmail.com fone:<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-telephone-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+		  <path fill-rule="evenodd" d="M2.267.98a1.636 1.636 0 0 1 2.448.152l1.681 2.162c.309.396.418.913.296 1.4l-.513 2.053a.636.636 0 0 0 .167.604L8.65 9.654a.636.636 0 0 0 .604.167l2.052-.513a1.636 1.636 0 0 1 1.401.296l2.162 1.681c.777.604.849 1.753.153 2.448l-.97.97c-.693.693-1.73.998-2.697.658a17.47 17.47 0 0 1-6.571-4.144A17.47 17.47 0 0 1 .639 4.646c-.34-.967-.035-2.004.658-2.698l.97-.969z"/>
 		</svg> 53999367651</p>
 	</div>
 </body>
