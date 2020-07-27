10b64ea50cf0f6601748b96a6630f1308604f586
fabioportovasques <fabioportovasques321@gmail.com>
2020-07-27 17:32:58 -0300

    add arquivos cad-epi,cad-fornecedor e alteração cad-func no botão de selecionar fotos

--- segware-epi/dashboard/dist/cad-fornecedor.php
+++ segware-epi/dashboard/dist/cad-fornecedor.php
@@ -71,7 +71,7 @@
                                 </svg>
                        &nbsp Trocar Usuário</a>
 
-                        <a class="dropdown-item"   href="#">
+                        <a class="dropdown-item"   href="http://localhost/tcc/segware-epi/sobre.php">
                         <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-bell-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z"/>
                         </svg>
@@ -85,7 +85,7 @@
 
                         
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="login.html">
+                        <a class="dropdown-item" href="http://localhost/tcc/segware-epi/index.php">
                         <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-box-arrow-in-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                               <path fill-rule="evenodd" d="M8.146 11.354a.5.5 0 0 1 0-.708L10.793 8 8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0z"/>
                               <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 1 8z"/>
